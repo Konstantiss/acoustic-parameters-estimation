528efdd9fd978756190d4d49aebd1733066775a4
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-10-16 12:02:32 +0300

    Add feature importance plot.

--- random_forest.py
+++ random_forest.py
@@ -40,7 +40,7 @@ print('Training Labels Shape:', train_labels.shape)
 print('Testing Features Shape:', test_features.shape)
 print('Testing Labels Shape:', test_labels.shape)
 
-rf = RandomForestRegressor(n_estimators=5000, random_state=42)
+rf = RandomForestRegressor(n_estimators=1000, random_state=42)
 rf.fit(train_features, train_labels)
 
 predictions = rf.predict(test_features)
@@ -51,3 +51,26 @@ print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
 mape = 100 * (errors / test_labels)
 accuracy = 100 - np.mean(mape)
 print('Accuracy:', round(accuracy, 2), '%.')
+
+# Get numerical feature importances
+importances = list(rf.feature_importances_)
+# List of tuples with variable and importance
+feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
+# Sort the feature importances by most important first
+feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+# Print out the feature and importances
+[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances]
+
+# Set the style
+plt.style.use('fivethirtyeight')
+# list of x locations for plotting
+x_values = list(range(len(importances)))
+# Make a bar chart
+plt.bar(x_values, importances, orientation = 'vertical')
+# Tick labels for x axis
+plt.xticks(x_values, feature_list, rotation='vertical')
+# Axis labels and title
+plt.ylabel('Importance')
+plt.xlabel('Variable')
+plt.title('Variable Importances')
+plt.show()
