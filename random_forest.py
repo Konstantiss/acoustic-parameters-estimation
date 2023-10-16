75f0f3da7a4da42d12663556676524d307e9bea2
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-10-16 11:52:48 +0300

    Add random forest regressor.

--- random_forest.py
+++ random_forest.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 
 PLOT_FEATURES = False
 
@@ -25,7 +26,8 @@ if PLOT_FEATURES:
 
 labels = np.array(features[['FBDRRMean(Ch)', 'FBT60Mean(Ch)']])
 
-features = features.drop(['FBDRRMean(Ch)', 'FBT60Mean(Ch)'], axis=1)
+#Drop label columns and string columns
+features = features.drop(['FBDRRMean(Ch)', 'FBT60Mean(Ch)', 'file', 'filename'], axis=1)
 
 feature_list = list(features.columns)
 
@@ -37,3 +39,15 @@ print('Training Features Shape:', train_features.shape)
 print('Training Labels Shape:', train_labels.shape)
 print('Testing Features Shape:', test_features.shape)
 print('Testing Labels Shape:', test_labels.shape)
+
+rf = RandomForestRegressor(n_estimators=5000, random_state=42)
+rf.fit(train_features, train_labels)
+
+predictions = rf.predict(test_features)
+errors = abs(predictions - test_labels)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+
+# Calculate mean absolute percentage error (MAPE)
+mape = 100 * (errors / test_labels)
+accuracy = 100 - np.mean(mape)
+print('Accuracy:', round(accuracy, 2), '%.')
