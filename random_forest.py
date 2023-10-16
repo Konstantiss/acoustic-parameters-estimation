9478800d80b919d90b019290f436f72deedb8a84
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-10-16 11:38:58 +0300

    Add option to plot features.

--- random_forest.py
+++ random_forest.py
@@ -1,21 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+PLOT_FEATURES = False
 
 features = pd.read_csv('features_and_ground_truth.csv', index_col=0)
 
-features['spectral_centroid'].plot(title='Spectral Centroid')
-plt.show()
+if PLOT_FEATURES:
+    features['spectral_centroid'].plot(title='Spectral Centroid')
+    plt.show()
 
-features['spectral_bandwidth'].plot(title='Spectral Bandwidth')
-plt.show()
+    features['spectral_bandwidth'].plot(title='Spectral Bandwidth')
+    plt.show()
 
-features['spectral_rolloff'].plot(title='Spectral Rolloff')
-plt.show()
+    features['spectral_rolloff'].plot(title='Spectral Rolloff')
+    plt.show()
 
-features['rmse'].plot(title='RMSE')
-plt.show()
+    features['rmse'].plot(title='RMSE')
+    plt.show()
 
-features['zero_crossing_rate'].plot(title='Zero Crossing Rate')
-plt.show()
+    features['zero_crossing_rate'].plot(title='Zero Crossing Rate')
+    plt.show()
 
 
