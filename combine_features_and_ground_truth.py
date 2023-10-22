5fa310f8fc74339d67e3a4be91d05668117cd1b2
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-10-22 18:03:51 +0300

    Cover all possible filenames in order to get the target_name right.

--- combine_features_and_ground_truth.py
+++ combine_features_and_ground_truth.py
@@ -3,7 +3,7 @@ from os import path
 from glob import glob
 import numpy as np
 
-DATA_PATH = '/home/konstantis/Nextcloud/ΤΗΜΜΥ/Thesis/Data/ACE/script-output/Dev/Speech/'
+DATA_PATH = '/home/konstantis/Nextcloud/ΤΗΜΜΥ/Thesis/Data/ACE/script-output/Eval/Speech/'
 
 features = pd.read_csv(path.join(DATA_PATH, 'audio_features.csv'))
 
@@ -13,7 +13,15 @@ features['FBT60Mean(Ch)'] = np.nan
 
 for i, row in features.iterrows():
     mic_type = row['filename'].split('_')[0]
-    target_name = row['filename'].split('_M')[0]
+    # The following solution is not elegant.
+    if row['filename'].find('1_M') != -1:
+        target_name = row['filename'].split('1_M')[0]
+    elif row['filename'].find('2_M') != -1:
+        target_name = row['filename'].split('2_M')[0]
+    elif row['filename'].find('1_F') != -1:
+        target_name = row['filename'].split('1_F')[0]
+    else:
+        target_name = row['filename'].split('2_F')[0]
     ground_truth_csv_path = glob(DATA_PATH + mic_type + '/*reduced.csv')[0]
     ground_truth_csv = pd.read_csv(ground_truth_csv_path)
     ground_truth_column = ground_truth_csv[ground_truth_csv['Filename'].str.contains(target_name)]
