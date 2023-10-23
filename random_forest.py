bdec52af52dc199abff1925ad4ff2f9b7fcc217e
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-10-23 14:07:14 +0300

    Change the metrics used for the model evaluation.

--- random_forest.py
+++ random_forest.py
@@ -4,10 +4,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_percentage_error, accuracy_score, mean_squared_error, r2_score, mean_absolute_error
 
 PLOT_FEATURES = False
 
-features = pd.read_csv('features_and_ground_truth.csv', index_col=0)
+features = pd.read_csv('features_and_ground_truth_eval.csv', index_col=0)
 
 if PLOT_FEATURES:
     features['spectral_centroid'].plot(title='Spectral Centroid')
@@ -41,22 +42,26 @@ print('Training Labels Shape:', train_labels.shape)
 print('Testing Features Shape:', test_features.shape)
 print('Testing Labels Shape:', test_labels.shape)
 
-rf = RandomForestRegressor(n_estimators=2000, random_state=42, n_jobs=-1, verbose=2)
+rf = RandomForestRegressor(n_estimators=250, random_state=42, n_jobs=-1, verbose=2)
 rf.fit(train_features, train_labels)
 print("Random forest parameters: ", rf.get_params())
 
 
 def evaluate(model, test_features, test_labels):
     predictions = model.predict(test_features)
-    errors = abs(predictions - test_labels)
-    mape = 100 * np.mean(errors / test_labels)
+    r_squared = r2_score(test_labels, predictions)
+    mae = mean_absolute_error(test_labels, predictions)
+    mse = mean_squared_error(test_labels, predictions)
+    mape = mean_absolute_percentage_error(test_labels, predictions)
     accuracy = 100 - mape
-    print('Model Performance')
-    print('Average Error: {:0.4f} degrees.'.format(np.mean(errors)))
-    print('Accuracy = {:0.2f}%.'.format(accuracy))
 
-    return accuracy
-
+    print('------Model Performance------')
+    print('R squared:', r_squared)
+    print('Mean Absolute Error (MAE):', mae)
+    print('Mean Squared Error (MSE):', mse)
+    print('Root Mean Squared Error (RMSE):', np.sqrt(mse))
+    # print('Mean Absolute Percentage Error (MAPE):', round(mape * 100, 2))
+    # print('Accuracy:', round(100 * accuracy, 2))
 
 evaluate(rf, test_features, test_labels)
 
@@ -86,7 +91,7 @@ if PLOT_VARIABLE_IMPORTANCES:
     plt.title('Variable Importances')
     plt.show()
 
-GRID_SEARCH = True
+GRID_SEARCH = False
 
 if GRID_SEARCH:
     param_grid = {
@@ -99,7 +104,7 @@ if GRID_SEARCH:
     }
     rf = RandomForestRegressor()
     grid_search = GridSearchCV(estimator=rf, param_grid=param_grid,
-                               cv=3, n_jobs=-1, verbose=2)
+                               cv=3, n_jobs=-1, verbose=-1)
     grid_search.fit(train_features, train_labels)
 
     print("Best parameters: ", grid_search.best_params_)
