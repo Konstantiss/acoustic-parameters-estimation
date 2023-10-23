11be0313810ee627eddd3480571ab66c4fd4b401
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-10-23 12:51:32 +0300

    Implement a more elegant solution.

--- combine_features_and_ground_truth.py
+++ combine_features_and_ground_truth.py
@@ -5,7 +5,12 @@ import numpy as np
 
 DATA_PATH = '/home/konstantis/Nextcloud/ΤΗΜΜΥ/Thesis/Data/ACE/script-output/Eval/Speech/'
 
-features = pd.read_csv(path.join(DATA_PATH, 'audio_features.csv'))
+features = pd.read_csv('audio_features_eval.csv')
+
+ROOMS_AND_MIC_CONFIGS = ['Office_1_1', 'Office_1_2', 'Office_2_1', 'Office_2_2', 'Meeting_Room_1_1', 'Meeting_Room_1_2',
+                         'Meeting_Room_1_2', 'Meeting_Room_2_1', 'Meeting_Room_2_2', 'Lecture_Room_1_1',
+                         'Lecture_Room_1_2',
+                         'Lecture_Room_2_1', 'Lecture_Room_2_2', 'Building_Lobby_1', 'Building_Lobby_2']
 
 # Add empty ground truth columns
 features['FBDRRMean(Ch)'] = np.nan
@@ -13,19 +18,12 @@ features['FBT60Mean(Ch)'] = np.nan
 
 for i, row in features.iterrows():
     mic_type = row['filename'].split('_')[0]
-    # The following solution is not elegant.
-    if row['filename'].find('1_M') != -1:
-        target_name = row['filename'].split('1_M')[0]
-    elif row['filename'].find('2_M') != -1:
-        target_name = row['filename'].split('2_M')[0]
-    elif row['filename'].find('1_F') != -1:
-        target_name = row['filename'].split('1_F')[0]
-    else:
-        target_name = row['filename'].split('2_F')[0]
+    room_and_mic_config = [substring for substring in ROOMS_AND_MIC_CONFIGS if substring in row['filename']][0]
+    target_name = mic_type + '_' + room_and_mic_config
     ground_truth_csv_path = glob(DATA_PATH + mic_type + '/*reduced.csv')[0]
     ground_truth_csv = pd.read_csv(ground_truth_csv_path)
     ground_truth_column = ground_truth_csv[ground_truth_csv['Filename'].str.contains(target_name)]
     features.at[i, 'FBDRRMean(Ch)'] = ground_truth_column['FBDRRMean(Ch)']
     features.at[i, 'FBT60Mean(Ch)'] = ground_truth_column['FBT60Mean(Ch)']
 
-features.to_csv(DATA_PATH + 'features_and_ground_truth.csv')
+features.to_csv(DATA_PATH + 'features_and_ground_truth_eval.csv')
