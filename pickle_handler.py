7ac8c69ca62a57a5ed836b0ca7ebeff26d3923d7
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-11-06 16:46:06 +0200

    Add handling of evaluation results.

--- pickle_handler.py
+++ pickle_handler.py
@@ -12,24 +12,41 @@ with (open(pickle_file_path, "rb")) as openfile:
         except EOFError:
             break
 
-mean_loss_per_epoch_drr = objects[0]['loss_drr']
-mean_loss_per_epoch_rt60 = objects[0]['loss_rt60']
-num_epochs = len(mean_loss_per_epoch_drr)
+mean_loss_per_epoch_train_drr = objects[0]['train_loss_drr']
+mean_loss_per_epoch_train_rt60 = objects[0]['train_loss_rt60']
+mean_loss_per_epoch_eval_drr = objects[0]['eval_loss_drr']
+mean_loss_per_epoch_eval_rt60 = objects[0]['eval_loss_rt60']
+num_epochs = len(mean_loss_per_epoch_train_drr)
 
 print('Model:', objects[0]['model'])
 print('Number of epochs:', num_epochs)
-print('DRR loss per epoch:', objects[0]['loss_drr'])
-print('RT60 loss per epoch:', objects[0]['loss_rt60'])
+print('DRR train loss per epoch:', mean_loss_per_epoch_train_drr)
+print('RT60 train loss per epoch:', mean_loss_per_epoch_train_rt60)
+print('DRR eval loss per epoch:', mean_loss_per_epoch_eval_drr)
+print('RT60 eval loss per epoch:', mean_loss_per_epoch_eval_rt60)
 print('Date and time:', objects[0]['datetime'])
 
 PLOT = True
 
 if PLOT:
-    plot_filename = 'figs/loss-plot-' + str(objects[0]['datetime']) + '.png'
+    plot_filename = 'figs/loss-plot-train-' + str(objects[0]['datetime']) + '.png'
     plt.figure(figsize=(10, 5))
-    plt.title("DRR and RT60 estimation loss per epoch")
-    plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_drr, linestyle='solid', marker='o', label="drr")
-    plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_rt60, linestyle='solid', marker='o', label="rt60")
+    plt.title("DRR and RT60 training loss per epoch")
+    plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_train_drr, linestyle='solid', marker='o', label="drr")
+    plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_train_rt60, linestyle='solid', marker='o', label="rt60")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.xlim(1, )
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.savefig(plot_filename)
+    plt.show()
+
+    plot_filename = 'figs/loss-plot-eval-' + str(objects[0]['datetime']) + '.png'
+    plt.figure(figsize=(10, 5))
+    plt.title("DRR and RT60 evaluation loss per epoch")
+    plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_eval_drr, linestyle='solid', marker='o', label="drr")
+    plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_eval_rt60, linestyle='solid', marker='o', label="rt60")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.xlim(1, )
