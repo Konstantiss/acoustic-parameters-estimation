76ad7e18c7478c723b4d2194b2cbfcd74bd76022
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-11-09 13:30:01 +0200

    Remove unused code.

--- train_CNN.py
+++ train_CNN.py
@@ -21,65 +21,6 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 print("Pytorch running on:", device)
 
-
-def train(model, train_dataloader, eval_dataloader, loss_fn, optimizer, device, epochs):
-    model = model.train()
-    for epoch in range(EPOCHS):
-        print("---- Training ---\n")
-        losses_per_epoch_train_drr = []
-        losses_per_epoch_train_rt60 = []
-        print("Learning rate: ", optimizer.param_groups[0]['lr'])
-        with tqdm.tqdm(train_dataloader, unit="batch", total=len(train_dataloader)) as tepoch:
-            for waveform, drrs_true, rt60s_true in tepoch:
-                tepoch.set_description(f"Epoch {epoch + 1}")
-                waveform = waveform.to(device)
-                drrs_true = drrs_true.to(device)
-                rt60s_true = rt60s_true.to(device)
-                # calculate loss and preds
-                drr_estimates, rt60_estimates = model(waveform)
-                loss_drr = loss_fn(drr_estimates.float(), drrs_true.float())
-                loss_rt60 = loss_fn(rt60_estimates.float(), rt60s_true.float())
-                losses_per_epoch_train_drr.append(loss_drr.item())
-                losses_per_epoch_train_rt60.append(loss_rt60.item())
-                # backpropogate the losses and update the gradients
-                optimizer.zero_grad()
-                loss_drr.backward(retain_graph=True)
-                loss_rt60.backward()
-                optimizer.step()
-                tepoch.set_postfix(loss_drr=loss_drr.item(), loss_rt60=loss_rt60.item())
-        print(f"Mean DRR training loss for epoch {epoch + 1}:",
-              sum(losses_per_epoch_train_drr) / len(losses_per_epoch_train_drr))
-        print(f"Mean RT60 training loss for epoch {epoch + 1}:",
-              sum(losses_per_epoch_train_rt60) / len(losses_per_epoch_train_rt60))
-        mean_loss_per_epoch_train_drr.append(sum(losses_per_epoch_train_drr) / len(losses_per_epoch_train_drr))
-        mean_loss_per_epoch_train_rt60.append(sum(losses_per_epoch_train_rt60) / len(losses_per_epoch_train_rt60))
-
-        print("---- Evaluation ---\n")
-
-        model = model.eval()
-        losses_per_epoch_eval_drr = []
-        losses_per_epoch_eval_rt60 = []
-        with tqdm.tqdm(eval_dataloader, unit="batch", total=len(eval_dataloader)) as tepoch:
-            for waveform, drrs_true, rt60s_true in tepoch:
-                tepoch.set_description(f"Epoch {epoch + 1}")
-                waveform = waveform.to(device)
-                drrs_true = drrs_true.to(device)
-                rt60s_true = rt60s_true.to(device)
-                # calculate loss and preds
-                drr_estimates, rt60_estimates = model(waveform)
-                loss_drr = loss_fn(drr_estimates.float(), drrs_true.float())
-                loss_rt60 = loss_fn(rt60_estimates.float(), rt60s_true.float())
-                losses_per_epoch_eval_drr.append(loss_drr.item())
-                losses_per_epoch_eval_rt60.append(loss_rt60.item())
-                tepoch.set_postfix(loss_drr=loss_drr.item(), loss_rt60=loss_rt60.item())
-        print(f"Mean DRR evaluation loss for epoch {epoch + 1}:",
-              sum(losses_per_epoch_eval_drr) / len(losses_per_epoch_eval_drr))
-        print(f"Mean RT60 evaluation loss for epoch {epoch + 1}:",
-              sum(losses_per_epoch_eval_rt60) / len(losses_per_epoch_eval_rt60))
-        mean_loss_per_epoch_eval_drr.append(sum(losses_per_epoch_eval_drr) / len(losses_per_epoch_eval_drr))
-        mean_loss_per_epoch_eval_rt60.append(sum(losses_per_epoch_eval_rt60) / len(losses_per_epoch_eval_rt60))
-
-
 DATA_PATH_TRAIN = '/home/konstantis/Nextcloud/ΤΗΜΜΥ/Thesis/Data/ACE/script-output/Train/Speech/'
 annotations_file_path_train = DATA_PATH_TRAIN + 'features_and_ground_truth_train.csv'
 
