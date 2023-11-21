72fe05628a40e638c7354474681f0d178cf6cb40
Konstantiss <konstantinos.michopoulos@gmail.com>
2023-11-21 19:14:20 +0200

    Evaluate model only once after training.

--- train_and_evaluate_model.py
+++ train_and_evaluate_model.py
@@ -80,40 +80,41 @@ def train_evaluate(model, train_dataloader, eval_dataloader, loss_fn, optimizer,
         last_loss_rt60 = current_epoch_loss_train_rt60
         mean_loss_per_epoch_train_drr.append(current_epoch_loss_train_drr)
         mean_loss_per_epoch_train_rt60.append(current_epoch_loss_train_rt60)
+        scheduler.step(current_epoch_loss_train_rt60)
 
-        print("---- Evaluation ---\n")
+    print("---- Evaluation ---\n")
 
-        model = model.eval()
-        losses_per_epoch_eval_drr = []
-        losses_per_epoch_eval_rt60 = []
-        with tqdm.tqdm(eval_dataloader, unit="batch", total=len(eval_dataloader)) as tepoch:
-            for waveform, drrs_true, rt60s_true in tepoch:
-                tepoch.set_description(f"Epoch {epoch + 1}")
-                waveform = waveform.to(device)
-                drrs_true = drrs_true.to(device)
-                rt60s_true = rt60s_true.to(device)
-                # calculate loss and preds
-                if model.__class__.__name__ == 'CNNNetwork':
-                    drr_estimates, rt60_estimates = model(waveform)
-                else:
-                    estimates = model(waveform)
-                    drr_estimates = estimates[:, 0]
-                    rt60_estimates = estimates[:, 1]
-                loss_drr = loss_fn(drr_estimates.float(), drrs_true.float())
-                loss_rt60 = loss_fn(rt60_estimates.float(), rt60s_true.float())
-                losses_per_epoch_eval_drr.append(loss_drr.item())
-                losses_per_epoch_eval_rt60.append(loss_rt60.item())
-                tepoch.set_postfix(loss_drr=loss_drr.item(), loss_rt60=loss_rt60.item())
-        current_epoch_loss_eval_drr = sum(losses_per_epoch_eval_drr) / len(losses_per_epoch_eval_drr)
-        current_epoch_loss_eval_rt60 = sum(losses_per_epoch_eval_rt60) / len(losses_per_epoch_eval_rt60)
-        print(f"Mean DRR evaluation loss for epoch {epoch + 1}:",
-              current_epoch_loss_eval_drr)
-        print(f"Mean RT60 evaluation loss for epoch {epoch + 1}:",
-              current_epoch_loss_eval_rt60)
+    model = model.eval()
+    losses_per_epoch_eval_drr = []
+    losses_per_epoch_eval_rt60 = []
+    with tqdm.tqdm(eval_dataloader, unit="batch", total=len(eval_dataloader)) as tepoch:
+        for waveform, drrs_true, rt60s_true in tepoch:
+            tepoch.set_description(f"Evaluation")
+            waveform = waveform.to(device)
+            drrs_true = drrs_true.to(device)
+            rt60s_true = rt60s_true.to(device)
+            # calculate loss and preds
+            if model.__class__.__name__ == 'CNNNetwork':
+                drr_estimates, rt60_estimates = model(waveform)
+            else:
+                estimates = model(waveform)
+                drr_estimates = estimates[:, 0]
+                rt60_estimates = estimates[:, 1]
+            loss_drr = loss_fn(drr_estimates.float(), drrs_true.float())
+            loss_rt60 = loss_fn(rt60_estimates.float(), rt60s_true.float())
+            losses_per_epoch_eval_drr.append(loss_drr.item())
+            losses_per_epoch_eval_rt60.append(loss_rt60.item())
+            tepoch.set_postfix(loss_drr=loss_drr.item(), loss_rt60=loss_rt60.item())
+    current_epoch_loss_eval_drr = sum(losses_per_epoch_eval_drr) / len(losses_per_epoch_eval_drr)
+    current_epoch_loss_eval_rt60 = sum(losses_per_epoch_eval_rt60) / len(losses_per_epoch_eval_rt60)
+    print(f"Mean DRR evaluation loss:",
+          current_epoch_loss_eval_drr)
+    print(f"Mean RT60 evaluation loss:",
+          current_epoch_loss_eval_rt60)
 
-        mean_loss_per_epoch_eval_drr.append(current_epoch_loss_eval_drr)
-        mean_loss_per_epoch_eval_rt60.append(current_epoch_loss_eval_rt60)
-        scheduler.step(current_epoch_loss_eval_rt60)
+    mean_loss_per_epoch_eval_drr.append(current_epoch_loss_eval_drr)
+    mean_loss_per_epoch_eval_rt60.append(current_epoch_loss_eval_rt60)
+    scheduler.step(current_epoch_loss_eval_rt60)
 
     return mean_loss_per_epoch_train_drr, mean_loss_per_epoch_train_rt60, mean_loss_per_epoch_eval_drr, \
            mean_loss_per_epoch_eval_rt60
