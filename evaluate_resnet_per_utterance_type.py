11c564475048b5c222dcffcc9373a1b4324fe3ba
Konstantiss <konstantinos.michopoulos@gmail.com>
2024-02-26 17:02:08 +0200

    Add evaluation per utterance type.

--- evaluate_resnet_per_utterance_type.py
+++ evaluate_resnet_per_utterance_type.py
@@ -38,10 +38,11 @@ transform = transforms.Compose([
 RESULTS_DIR = '/home/konstantis/Nextcloud/ΤΗΜΜΥ/Thesis/Results/'
 
 DATA_PATH_TRAIN = '/home/konstantis/Nextcloud/ΤΗΜΜΥ/Thesis/Data/ACE/script-output/Train/Speech/'
-annotations_file_path_train = DATA_PATH_TRAIN + 'features_and_ground_truth_train.csv'
 
 DATA_PATH_EVAL = '/home/konstantis/Nextcloud/ΤΗΜΜΥ/Thesis/Data/ACE/script-output/Eval/Speech/'
-annotations_file_path_eval = DATA_PATH_EVAL + 'features_and_ground_truth_eval.csv'
+
+annotation_file_names = ['s1_utterances_train.csv', 's2_utterances_train.csv', 's3_utterances_train.csv',
+                         's4_utterances_train.csv', 's5_utterances_train.csv']
 
 SAMPLE_RATE = 22050
 NUM_SAMPLES = 22050
@@ -49,12 +50,6 @@ BATCH_SIZE = 256
 EPOCHS = 30
 
 melspectogram = ta.transforms.MelSpectrogram(sample_rate=SAMPLE_RATE, n_fft=1024, hop_length=512, n_mels=64)
-train_dataset = ACEDataset(annotations_file_path_train, melspectogram, SAMPLE_RATE, NUM_SAMPLES, device, resnet=True,
-                           image_transformation=transform)
-train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-eval_dataset = ACEDataset(annotations_file_path_eval, melspectogram, SAMPLE_RATE, NUM_SAMPLES, device, resnet=True,
-                          image_transformation=transform)
-eval_dataloader = DataLoader(eval_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 model = models.resnet18(pretrained=True).to(device)
 
@@ -67,27 +62,58 @@ model.fc = nn.Linear(model.fc.in_features, 2, device=device)
 model.load_state_dict(torch.load(RESULTS_DIR + 'resnet-save-2023-11-23 155931.877396-30.bin'))
 
 loss_fn = torch.nn.MSELoss()
-# optimizer = torch.optim.SGD(model.parameters(), lr=10e-6, momentum=0.9)
 optimizer = torch.optim.Adam(model.parameters(), lr=10e-4)
 start_time = time.time()
 
-mean_loss_per_epoch_eval_drr, mean_loss_per_epoch_eval_rt60 = evaluate(model=model, eval_dataloader=eval_dataloader, loss_fn=loss_fn, device=device)
+losses_drr = []
+losses_rt60 = []
+
+for i, file in enumerate(annotation_file_names):
+    annotation_file_path = DATA_PATH_TRAIN + file
+    eval_dataset = ACEDataset(annotation_file_path, melspectogram, SAMPLE_RATE, NUM_SAMPLES, device, resnet=True,
+                              image_transformation=transform)
+    eval_dataloader = DataLoader(eval_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    loss_drr, loss_rt60 = evaluate(model=model, eval_dataloader=eval_dataloader,
+                                   loss_fn=loss_fn, device=device)
+    print("S"+ str(i + 1) +" utterance loss DRR:", loss_drr)
+    print("S" + str(i + 1) + " utterance loss RT60:", loss_rt60)
+    losses_drr.append(loss_drr)
+    losses_rt60.append(loss_rt60)
 
 execution_time = (time.time() - start_time) / 60
 date_time = str(datetime.datetime.now())
 
+s1_utterance_loss_drr = losses_drr[0]
+s1_utterance_loss_rt60 = losses_rt60[0]
+s2_utterance_loss_drr = losses_drr[1]
+s2_utterance_loss_rt60 = losses_rt60[1]
+s3_utterance_loss_drr = losses_drr[2]
+s3_utterance_loss_rt60 = losses_rt60[2]
+s4_utterance_loss_drr = losses_drr[3]
+s4_utterance_loss_rt60 = losses_rt60[3]
+s5_utterance_loss_drr = losses_drr[4]
+s5_utterance_loss_rt60 = losses_rt60[4]
+
 results = {
     "model": model.__class__.__name__,
-    "eval_loss_drr": mean_loss_per_epoch_eval_drr,
-    "eval_loss_rt60": mean_loss_per_epoch_eval_rt60,
+    "s1_utterance_loss_drr": s1_utterance_loss_drr,
+    "s1_utterance_loss_rt60": s1_utterance_loss_rt60,
+    "s2_utterance_loss_drr": s2_utterance_loss_drr,
+    "s2_utterance_loss_rt60": s2_utterance_loss_rt60,
+    "s3_utterance_loss_drr": s3_utterance_loss_drr,
+    "s3_utterance_loss_rt60": s3_utterance_loss_rt60,
+    "s4_utterance_loss_drr": s4_utterance_loss_drr,
+    "s4_utterance_loss_rt60": s4_utterance_loss_rt60,
+    "s5_utterance_loss_drr": s5_utterance_loss_drr,
+    "s5_utterance_loss_rt60": s5_utterance_loss_rt60,
     "datetime": datetime.datetime.now(),
     "execution_time": execution_time
 }
 
 print('Total execution time: {:.4f} minutes', format(execution_time))
-print("Evaluation loss DRR:", mean_loss_per_epoch_eval_drr)
-print("Evaluation loss RT60:", mean_loss_per_epoch_eval_rt60)
+print("Evaluation loss DRR:", losses_drr)
+print("Evaluation loss RT60:", losses_rt60)
 
-results_filename = RESULTS_DIR + 'results-resnet-' + date_time + '-' + str(EPOCHS) + '.pkl'
-# with open(results_filename, 'wb') as handle:
-#     pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
+results_filename = RESULTS_DIR + 'results-resnet-per-utterance-' + date_time + '-' + str(EPOCHS) + '.pkl'
+with open(results_filename, 'wb') as handle:
+    pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
