4f5d44e792cd182f13aba0ce50c00ddb0b17af63
Konstantiss <konstantinos.michopoulos@gmail.com>
2024-02-26 17:02:37 +0200

    Handle results per utterance.

--- pickle_handler.py
+++ pickle_handler.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 RESULTS_DIR = '/home/konstantis/Nextcloud/ΤΗΜΜΥ/Thesis/Results/'
 
-pickle_file_path = RESULTS_DIR + 'results-resnet2023-11-22 13:49:08.831606-1.pkl'
+pickle_file_path = RESULTS_DIR + 'results-resnet-per-utterance-2024-02-26 16:59:39.587180-30.pkl'
 
 pkl_contents = []
 with (open(pickle_file_path, "rb")) as openfile:
@@ -13,51 +13,68 @@ with (open(pickle_file_path, "rb")) as openfile:
         except EOFError:
             break
 
-mean_loss_per_epoch_train_drr = pkl_contents[0]['train_loss_drr']
-mean_loss_per_epoch_train_rt60 = pkl_contents[0]['train_loss_rt60']
-mean_loss_per_epoch_eval_drr = pkl_contents[0]['eval_loss_drr']
-mean_loss_per_epoch_eval_rt60 = pkl_contents[0]['eval_loss_rt60']
+if 'per-utterance' in pickle_file_path:
+    print("---- Results per utterance ----")
+    print('Model:', pkl_contents[0]['model'])
+    print('S1 utterance loss DRR:', pkl_contents[0]['s1_utterance_loss_drr'])
+    print('S1 utterance loss RT60:', pkl_contents[0]['s1_utterance_loss_rt60'])
+    print('S2 utterance loss DRR:', pkl_contents[0]['s2_utterance_loss_drr'])
+    print('S2 utterance loss RT60:', pkl_contents[0]['s2_utterance_loss_rt60'])
+    print('S3 utterance loss DRR:', pkl_contents[0]['s3_utterance_loss_drr'])
+    print('S3 utterance loss RT60:', pkl_contents[0]['s3_utterance_loss_rt60'])
+    print('S4 utterance loss DRR:', pkl_contents[0]['s4_utterance_loss_drr'])
+    print('S4 utterance loss RT60:', pkl_contents[0]['s4_utterance_loss_rt60'])
+    print('S5 utterance loss DRR:', pkl_contents[0]['s5_utterance_loss_drr'])
+    print('S5 utterance loss RT60:', pkl_contents[0]['s5_utterance_loss_rt60'])
+    print('Date and time:', pkl_contents[0]['datetime'])
+    print('Execution time:', pkl_contents[0]['execution_time'])
+    model_name = pkl_contents[0]['model']
+else:
+    mean_loss_per_epoch_train_drr = pkl_contents[0]['train_loss_drr']
+    mean_loss_per_epoch_train_rt60 = pkl_contents[0]['train_loss_rt60']
+    mean_loss_per_epoch_eval_drr = pkl_contents[0]['eval_loss_drr']
+    mean_loss_per_epoch_eval_rt60 = pkl_contents[0]['eval_loss_rt60']
 
-num_epochs = len(mean_loss_per_epoch_train_drr)
-#model_name = 'cnn' if pkl_contents[0]['model'] == 'CNNNetwork' else 'resnet'
-model_name = pkl_contents[0]['model']
+    num_epochs = len(mean_loss_per_epoch_train_drr)
+    #model_name = 'cnn' if pkl_contents[0]['model'] == 'CNNNetwork' else 'resnet'
+    model_name = pkl_contents[0]['model']
 
-print('Model:', pkl_contents[0]['model'])
-print('Number of epochs:', num_epochs)
-print('DRR train loss per epoch:', mean_loss_per_epoch_train_drr)
-print('RT60 train loss per epoch:', mean_loss_per_epoch_train_rt60)
-print('DRR evaluation loss per epoch:', mean_loss_per_epoch_eval_drr)
-print('RT60 evaluation loss per epoch:', mean_loss_per_epoch_eval_rt60)
-print('Date and time:', pkl_contents[0]['datetime'])
-print('Execution time:', pkl_contents[0]['execution_time'])
+    print('Model:', pkl_contents[0]['model'])
+    print('Number of epochs:', num_epochs)
+    print('DRR train loss per epoch:', mean_loss_per_epoch_train_drr)
+    print('RT60 train loss per epoch:', mean_loss_per_epoch_train_rt60)
+    print('DRR evaluation loss per epoch:', mean_loss_per_epoch_eval_drr)
+    print('RT60 evaluation loss per epoch:', mean_loss_per_epoch_eval_rt60)
+    print('Date and time:', pkl_contents[0]['datetime'])
+    print('Execution time:', pkl_contents[0]['execution_time'])
 
-PLOT = True
+    PLOT = True
 
-if PLOT:
-    plot_filename = RESULTS_DIR + 'figs/' + model_name + '-loss-plot-train-' + str(pkl_contents[0]['datetime']) + '-' + str(num_epochs) + '.png'
-    plt.figure(figsize=(10, 5))
-    plt.title(model_name + "DRR and RT60 training loss per epoch")
-    plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_train_drr, linestyle='solid', marker='o', label="drr")
-    plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_train_rt60, linestyle='solid', marker='o', label="rt60")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.xlim(1, )
-    plt.ylim(0, 1)
-    plt.legend()
-    plt.savefig(plot_filename)
-    plt.show()
+    if PLOT:
+        plot_filename = RESULTS_DIR + 'figs/' + model_name + '-loss-plot-train-' + str(pkl_contents[0]['datetime']) + '-' + str(num_epochs) + '.png'
+        plt.figure(figsize=(10, 5))
+        plt.title(model_name + "DRR and RT60 training loss per epoch")
+        plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_train_drr, linestyle='solid', marker='o', label="drr")
+        plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_train_rt60, linestyle='solid', marker='o', label="rt60")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.xlim(1, )
+        plt.ylim(0, 1)
+        plt.legend()
+        plt.savefig(plot_filename)
+        plt.show()
 
-    # plot_filename = RESULTS_DIR + 'figs/' + model_name + 'loss-plot-eval-' + str(pkl_contents[0]['datetime']) + '-' + str(num_epochs) + '.png'
-    # plt.figure(figsize=(10, 5))
-    # plt.title(model_name + " DRR and RT60 evaluation loss per epoch")
-    # plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_eval_drr, linestyle='solid', marker='o', label="drr")
-    # plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_eval_rt60, linestyle='solid', marker='o', label="rt60")
-    # plt.xlabel("Epoch")
-    # plt.ylabel("Loss")
-    # plt.xlim(1, )
-    # plt.ylim(0, 1)
-    # plt.legend()
-    # plt.savefig(plot_filename)
-    # plt.show()
+        # plot_filename = RESULTS_DIR + 'figs/' + model_name + 'loss-plot-eval-' + str(pkl_contents[0]['datetime']) + '-' + str(num_epochs) + '.png'
+        # plt.figure(figsize=(10, 5))
+        # plt.title(model_name + " DRR and RT60 evaluation loss per epoch")
+        # plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_eval_drr, linestyle='solid', marker='o', label="drr")
+        # plt.plot(range(1, num_epochs + 1), mean_loss_per_epoch_eval_rt60, linestyle='solid', marker='o', label="rt60")
+        # plt.xlabel("Epoch")
+        # plt.ylabel("Loss")
+        # plt.xlim(1, )
+        # plt.ylim(0, 1)
+        # plt.legend()
+        # plt.savefig(plot_filename)
+        # plt.show()
 
 
